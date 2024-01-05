502f5a42c871efeb13261d75216232edc3cd4097
shaikhhamza29 <hamzarajmohammed29@gmail.com>
2024-01-05 23:29:21 +0530

    KINDSOFDATA ADDED

---
+++ kindsofdata.java
@@ -0,0 +1,19 @@
+class Data{
+    public static void main(String[] args) {
+        long L = 0L;
+        char ch = 'A';
+        int a = 10;
+        float f = 10.23f;
+        double d = 0.15D;
+        short s = 20;
+
+        System.out.println("long is "+L);
+        System.out.println("var is "+ch);
+        System.out.println("int is "+a);
+        System.out.println("float is "+f);
+        System.out.println("double is "+d);
+        System.out.println("short is "+s);        
+
+
+    }
+}
