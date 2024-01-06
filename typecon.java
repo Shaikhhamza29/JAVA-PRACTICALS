def69fca85f126bb4f3b390916058fe0b4e4e42c
shaikhhamza29 <hamzarajmohammed29@gmail.com>
2024-01-06 19:40:51 +0530

    TYPE CONVERSION ADDED

---
+++ typecon.java
@@ -0,0 +1,24 @@
+class typecon{
+	public static void main(String arg[]){
+		byte b = 90;
+		short s;
+		int c;
+			
+		s=b;
+		c=b;
+		System.out.println("BYTE IS : "+b);
+		System.out.println("SHORT IS : "+s);
+		System.out.println("INTEGER IS : "+c);
+		
+
+		int c1;
+		c1=10;
+	  
+	  byte b1= (byte) c1;
+		short s1=(short) c1;
+    
+    System.out.println("BYTE IS : "+b1);
+		System.out.println("SHORT IS : "+s1);
+		System.out.println("INTEGER IS : "+c1);
+		}
+	}
