e4b92181d014fc216633d2ed558a186d05c1a3a3
shaikhhamza29 <hamzarajmohammed29@gmail.com>
2024-01-06 21:07:55 +0530

    ISLLUSTRATE

---
+++ illustrate.java
@@ -0,0 +1,25 @@
+class illustrate{
+
+	public static void main(String args[]){
+	    byte s1 = 10;
+	    short s = 20;
+	    System.out.println(s+s1);
+	    System.out.println(s-s1);
+	    System.out.println(s*s1);
+	    System.out.println(s/s1);
+	    System.out.println(s%s1);
+	    System.out.println(s>s1);
+	    System.out.println(s>=s1);
+	    System.out.println(s<s1);
+	    System.out.println(s<=s1);
+	    System.out.println(s!=s1);
+
+      System.out.println(s>>s1);
+      System.out.println(s<<s1);
+      System.out.println(s>>>s1);
+    //  System.out.println(s&&s1);
+    //  System.out.println(s||s1);
+      System.out.println(s&s1);
+      //System.out.println(s!s1);
+	}
+}
