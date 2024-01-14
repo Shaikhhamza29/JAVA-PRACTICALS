acd1a75c7e1c357be542d0b5ac24981cd6595068
Shaikhhamza29 <hamzarajmohammed29@gmail.com>
2024-01-14 22:17:22 +0530

    Example2

---
+++ Example2.java
@@ -0,0 +1,12 @@
+class Example2
+{
+    public static void main(String args[])
+    {
+        int num;
+        num = 100;
+        System.out.println("The number is : "+num);
+
+        num = num * 2;
+        System.out.println("The number is : "+ num);
+    }
+}
