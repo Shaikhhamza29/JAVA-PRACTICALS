fa99232395975b86c3ce74885c813f5479088705
Shaikhhamza29 <hamzarajmohammed29@gmail.com>
2024-01-18 20:58:34 +0530

    CHARDEMO2

---
+++ chardemo2.java
@@ -0,0 +1,10 @@
+class chardemo2 
+{
+    public static void main(String args[]) {
+        char ch1;
+        ch1 = 'X';
+        System.out.println("Ch1 contains :"+ch1);
+        ch1 ++;
+        System.out.println("ch1 contains :"+ch1);
+    }    
+}
