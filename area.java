6a6855f4227b553c0c0f255976bf883015efc44a
Shaikhhamza29 <hamzarajmohammed29@gmail.com>
2024-01-18 20:56:19 +0530

    AREA

---
+++ area.java
@@ -0,0 +1,11 @@
+class area 
+{
+    public static void main(String[] args)
+    {
+        double pi , r , a ;
+        pi = 3.1416 ;
+        r = 10.8;
+        a = pi * r * r ;
+        System.out.println("AREA OF THE CIRCLE IS : "+a);        
+    }    
+}
