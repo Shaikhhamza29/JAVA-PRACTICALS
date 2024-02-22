c3d40827ae29a6a338d2f908cf5a5add6701be87
Shaikhhamza29 <hamzarajmohammed29@gmail.com>
2024-02-22 23:56:55 +0530

    NESTING TEST

---
+++ NestingTest.java
@@ -0,0 +1,26 @@
+class Nesting{
+    int m ,n ;
+    Nesting(int x,int y){
+        m = x;
+        n = y;
+    }
+    int largest(){
+        if( m >= n ){
+            return (m);
+        }
+        else{
+            return (n);
+        }
+    }
+    void display(){
+        int large = largest();
+        System.out.println("LARGEST VALUE IS : "+ large);
+    }
+}
+
+class NestingTest {
+    public static void main(String args[]) {
+        Nesting nest = new Nesting(50 , 60);
+        nest.display();
+    }    
+}
