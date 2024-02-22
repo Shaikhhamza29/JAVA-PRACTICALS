9aa954355bc192a4e12f0b0aa116e719bb914479
Shaikhhamza29 <hamzarajmohammed29@gmail.com>
2024-02-22 23:57:30 +0530

    RECTANGLE

---
+++ Rectangle.java
@@ -0,0 +1,25 @@
+class Rectangle {
+    int length , width ;
+    void GetData(int x , int y ){ 
+        length = x;
+        width = y;
+    }
+    int rectArea(){
+        int area = length * width;
+        return (area);
+    }
+}
+class RectArea{
+    public static void main(String args[]){
+        int area1 , area2;
+        Rectangle rect1 = new Rectangle();
+        Rectangle rect2 = new Rectangle();
+        rect1.length = 15;
+        rect1.width = 10;
+        area1 = rect1.length * rect1.width;
+        rect2.GetData(20, 12);
+        area2 = rect2.rectArea();
+        System.out.println("AREA1 = "+area1);
+        System.out.println("AREA2 = "+area2);
+    }
+}
